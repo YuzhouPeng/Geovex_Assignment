f079012d2f504c63d773b0ccb7def8d6627942c0
macjiong <760783896@qq.com>
2018-10-29 11:35:11 +0000

    add code

--- buildinginfo.py
+++
@@ -1,7 +0,0 @@
-class BuildingData(object):
-
-    def __init__(self,buildingid, residentialtype, buildyear, unitids):
-        self.buildingid = buildingid
-        self.usetype = residentialtype
-        self.buildyear = buildyear
-        self.unitids = unitids
