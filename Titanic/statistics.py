348a7fb1cfcac278fda1aad4c557d7cc826c65d1
macjiong <760783896@qq.com>
2019-04-07 18:22:28 +0800

    add code

--- Titanic/statistics.py
+++ Titanic/statistics.py
@@ -50,3 +50,56 @@ plt.title(u"各乘客等级的获救情况")
 plt.xlabel(u"乘客等级")
 plt.ylabel(u"人数")
 plt.show()
+
+
+fig = plt.figure()
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+
+Survived_0 = data_train.Pclass[data_train.Survived == 0].value_counts()
+Survived_1 = data_train.Pclass[data_train.Survived == 1].value_counts()
+df=pd.DataFrame({u'获救':Survived_1, u'未获救':Survived_0})
+df.plot(kind='bar', stacked=True)
+plt.title(u"各乘客等级的获救情况")
+plt.xlabel(u"乘客等级")
+plt.ylabel(u"人数")
+plt.show()
+
+
+fig = plt.figure()
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+
+Survived_m = data_train.Survived[data_train.Sex == 'male'].value_counts()
+Survived_f = data_train.Survived[data_train.Sex == 'female'].value_counts()
+df=pd.DataFrame({u'男性':Survived_m, u'女性':Survived_f})
+df.plot(kind='bar', stacked=True)
+plt.title(u"按性别看获救情况")
+plt.xlabel(u"性别")
+plt.ylabel(u"人数")
+plt.show()
+
+
+fig=plt.figure()
+fig.set(alpha=0.65) # 设置图像透明度，无所谓
+plt.title(u"根据舱等级和性别的获救情况")
+
+ax1=fig.add_subplot(141)
+data_train.Survived[data_train.Sex == 'female'][data_train.Pclass != 3].value_counts().plot(kind='bar', label="female highclass", color='#FA2479')
+ax1.set_xticklabels([u"获救", u"未获救"], rotation=0)
+ax1.legend([u"女性/高级舱"], loc='best')
+
+ax2=fig.add_subplot(142, sharey=ax1)
+data_train.Survived[data_train.Sex == 'female'][data_train.Pclass == 3].value_counts().plot(kind='bar', label='female, low class', color='pink')
+ax2.set_xticklabels([u"未获救", u"获救"], rotation=0)
+plt.legend([u"女性/低级舱"], loc='best')
+
+ax3=fig.add_subplot(143, sharey=ax1)
+data_train.Survived[data_train.Sex == 'male'][data_train.Pclass != 3].value_counts().plot(kind='bar', label='male, high class',color='lightblue')
+ax3.set_xticklabels([u"未获救", u"获救"], rotation=0)
+plt.legend([u"男性/高级舱"], loc='best')
+
+ax4=fig.add_subplot(144, sharey=ax1)
+data_train.Survived[data_train.Sex == 'male'][data_train.Pclass == 3].value_counts().plot(kind='bar', label='male low class', color='steelblue')
+ax4.set_xticklabels([u"未获救", u"获救"], rotation=0)
+plt.legend([u"男性/低级舱"], loc='best')
+
+plt.show()
